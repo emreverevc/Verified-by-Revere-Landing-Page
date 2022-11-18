8f78ae3d6e312abda01fc9cc25e9f90e2ffc08a4
E <emercker6275@gmail.com>
2022-11-17 20:36:40 -0800

    botton formatting

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 function dropdown(question_id) {
-    console.log("hello");
-    console.log(question_id);
     var answer = document.getElementById(question_id + "-answer");
     if (answer.style.maxHeight){
         answer.style.maxHeight = null;
