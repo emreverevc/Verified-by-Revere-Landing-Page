7567a36365a495062476e2e0e85379ff7f4cbbba
E <emercker6275@gmail.com>
2022-11-17 19:53:08 -0800

    dropdowns working?

--- main.js
+++ main.js
@@ -1,27 +1,10 @@
-function dropdown() {
-    var row = document.getElementsByClassName("accordian-item");
-    var i;
-  
-    for (i = 0; i < row.length; i++) {
-      row[i].addEventListener("click", function() {
-        
-        var blah = document.getElementsByClassName("row");
-            for (w = 0; w < blah.length; w++) {
-                if (row[w].classList.contains("row_selected")){
-                    if (row[w] == this) {
-                    
-                    } else {
-                    row[w].classList.remove("row_selected");
-                    var blahblah = row[w].nextElementSibling;
-                    if (blahblah.style.maxHeight){
-                        blahblah.style.maxHeight = null;
-                    } else {
-                        blahblah.style.maxHeight = blahblah.scrollHeight + "px";
-                    }
-                    }
-                }
-            }
-            this.classList.toggle("row_selected");
-        });
+function dropdown(question_id) {
+    console.log("hello");
+    console.log(question_id);
+    var answer = document.getElementById(question_id + "-answer");
+    if (answer.style.maxHeight){
+        answer.style.maxHeight = null;
+    } else {
+        answer.style.maxHeight = answer.scrollHeight + "px";
     }
 }
